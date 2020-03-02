460d431688d36baf256825c2d72e707cee73ef79
Wolfgang Welz <welzwo@gmail.com>
2020-03-02 20:58:58 +0100

    use bip39

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/wollac/iota-bip39-demo
 go 1.14
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/iotaledger/iota.go v1.0.0-beta.14
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.5.1
+	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5
+	golang.org/x/text v0.3.0
 )
--- main.go
+++ main.go
@@ -4,28 +4,59 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/iotaledger/iota.go/consts"
+	"github.com/iotaledger/iota.go/kerl"
+	"github.com/iotaledger/iota.go/trinary"
 	flag "github.com/spf13/pflag"
 	"github.com/wollac/iota-bip39-demo/bip32path"
+	"github.com/wollac/iota-bip39-demo/bip39"
 	"github.com/wollac/iota-bip39-demo/slip10"
 )
 
 var (
-	seed       = flag.BytesHex("seed", []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, "bytes of the master seed (hex)")
-	pathString = flag.String("path", "44'/4218'/0'/0'", "string form of the BIP-32 path to derive the extended private key")
+	mnemonicString = flag.String("mnemonic", "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about", "BIP-39 mnemonic sentence")
+	passphrase     = flag.String("passphrase", "", "optional passphrase")
+	pathString     = flag.String("path", "44'/4218'/0'/0'", "string form of the BIP-32 path to derive the extended private key")
 )
 
 func main() {
 	flag.Parse()
+
+	mnemonic := bip39.ParseMnemonic(*mnemonicString)
+	seed := bip39.MnemonicToSeed(mnemonic, *passphrase)
 
 	path, err := bip32path.ParsePath(*pathString)
 	if err != nil {
 		log.Fatalf("invalid path (%s): %s", *pathString, err)
 	}
 
+	kerl.NewKerl()
 	curve := slip10.Ed25519()
-	key, _ := slip10.DeriveKeyFromPath(*seed, curve, path)
+	key, _ := slip10.DeriveKeyFromPath(seed, curve, path)
 
+	fmt.Printf("master seed: %x\n", seed)
 	fmt.Printf("chain code: %x\n", key.ChainCode)
 	fmt.Printf("private: %x\n", key.Key)
 	fmt.Printf("public: %x\n", curve.PublicKey(key))
+	fmt.Printf("IOTA seed: %s\n", iotaSeedFromKey(key))
+}
+
+func iotaSeedFromKey(key *slip10.Key) trinary.Hash {
+	var entropy []byte
+	entropy = append(entropy, key.Key...)
+	entropy = append(entropy, key.ChainCode...)
+
+	hash := kerl.NewKerl()
+	in, err := kerl.KerlBytesToTrytes(entropy[:consts.HashBytesSize])
+	if err != nil {
+		panic(err)
+	}
+	hash.MustAbsorbTrytes(in)
+	in, err = kerl.KerlBytesToTrytes(entropy[len(entropy)-consts.HashBytesSize:])
+	if err != nil {
+		panic(err)
+	}
+	hash.MustAbsorbTrytes(in)
+
+	return hash.MustSqueezeTrytes(consts.HashTrinarySize)
 }
